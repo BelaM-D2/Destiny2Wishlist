dc332c1e4fb8f36faca03466cc3d6e80e0f0f12b
Your Name <bela.raphael.maurer@gmail.com>
2024-12-05 16:10:22 +0100

    Added 6.5 Pages of Weapon Ids

--- obj/Debug/net8.0/Destiny2Wishlist.AssemblyInfo.cs
+++ obj/Debug/net8.0/Destiny2Wishlist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d61ff7cea450df09bfbec97b24966e82b909be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47db31e13255676899b5bb7561d6ebdbe3b0be3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
