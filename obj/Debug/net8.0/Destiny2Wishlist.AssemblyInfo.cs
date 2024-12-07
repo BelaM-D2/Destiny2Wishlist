df6d838c960dd54de0cf89a764de04c39d2a55c5
Your Name <bela.raphael.maurer@gmail.com>
2024-12-07 18:21:53 +0100

    Added ChromaRush

--- obj/Debug/net8.0/Destiny2Wishlist.AssemblyInfo.cs
+++ obj/Debug/net8.0/Destiny2Wishlist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124e9e62fe21e3640236a4ca27b4b54d7210a954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca77618d6b0511e5133bcc339de9efe50ba0096")]
 [assembly: System.Reflection.AssemblyProductAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
