cf63fa6073754636ce249b0f26fef52ff9057a8f
Your Name <bela.raphael.maurer@gmail.com>
2024-12-09 19:46:08 +0100

    39/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/91739/917

--- obj/Debug/net8.0/Destiny2Wishlist.AssemblyInfo.cs
+++ obj/Debug/net8.0/Destiny2Wishlist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f316e0d4ee43f157cc1a36dff3bd7097f486aa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0432fc656ccb78eeffba6236b6e60b5f499d625")]
 [assembly: System.Reflection.AssemblyProductAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Destiny2Wishlist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
